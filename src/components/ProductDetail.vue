5b285fa8071068f6b67209ec35ce426ebe653606
tbrzc-sena <javavillabon@soy.sena.edu.co>
2024-08-15 14:07:12 -0500

    tremendo

--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -1,64 +1,64 @@
-<script>
+<script setup>
 import HeaderAlternative from '../views/base/HeaderAlternative.vue';
+import { useRoute } from 'vue-router';
+const route = useRoute();
+import gql from "graphql-tag";
+import { useQuery } from "@vue/apollo-composable";
 
-export default {
-  name: 'ProductDetail',
-  data() {
-    return {
-      product: {
-        idProducto: 0,
-        precio: 0,
-        imagenProducto: '',
-        modeloVehiculo: {
-          idModeloVehiculo: 0,
-          nombreModeloVehiculo: '',
-          numeroModeloVehiculo: 0,
-          marcaVehiculo: {
-            idMarcaVehiculo: 0,
-            nombreMarcaVehiculo: ''
-          },
-          tipoVehiculo: {
-            idTipoVehiculo: 0,
-            nombreTipoVehiculo: ''
-          }
-        },
-        categoria: {
-          idCategoria: 0,
-          nombreCategoria: '',
-          descuentoCategoria: 0
-        },
-        articulo: {
-          idArticulo: 0,
-          nombreArticulo: '',
-          descripcion: '',
-          cantidad: 0,
-          estadoArticulo: {
-            idEstadoArticulo: 0,
-            nombreEstadoArticulo: ''
-          },
-          tipoArticulo: {
-            idTipoArticulo: 0,
-            nombreTipoArticulo: ''
-          }
-        }
-      }
-
+const productId = route.params.id
+const decodedID = atob(productId).split(":")[1]
+import { ref, onBeforeMount,computed } from "vue";
+const GET_PRODUCT_BY_ID_QUERY = gql`query getProductById($id:ID!) {
+  product(id: $id) {
+    price
+    imageLink
+    inventoryItem {
+      name
+      stock
+      type
+      description
     }
-  },
-  methods: {
-    async getProduct() {
-      const response = await fetch(`http://localhost:8090/api/v1/productos/${this.$route.params.id}`)
-      const json = await response.json()
-      this.product = json
+    category {
+      name
+      discount
     }
-  },
-  beforeMount() {
-    this.getProduct()
-  },
-  components:{
-    HeaderAlternative
+    carModel {
+      name
+    }
   }
-}
+}`;
+
+let producto = ref({
+  precio: 0, //price
+  imagen: "", //imageLink
+  nombre: "", //itemName
+  descripcion: "", //description
+  cantidad: 0, //itemStock
+  tipoArticulo: 0, //itemType
+  modeloVehiculo: 0, //carModel
+  //marcaVehiculo: 0,//modelMake
+  categoria: 0, //category
+  //tipoVehiculo: 0,//modelType
+});
+
+const { result, loading, error } = useQuery(GET_PRODUCT_BY_ID_QUERY, () => ({ id: decodedID }));
+
+onBeforeMount(() => {
+  if (!loading.value && !error.value && result.value) {
+    const productData = result.value.product;
+    producto.value = {
+      precio: productData.price,
+      imagen: productData.imageLink,
+      nombre: productData.inventoryItem.name,
+      descripcion: productData.inventoryItem.description,
+      cantidad: productData.inventoryItem.stock,
+    };
+  }
+
+})
+
+const imageComputed = computed(() => producto.value.imagen);
+
 </script>
 <template>
   <!--
@@ -75,26 +75,28 @@ export default {
     <div class="mx-auto mt-6 max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="-mx-4 flex flex-col md:flex-row">
         <div class="border-2 border-solid border-sky-500 px-4 md:flex-1">
-          <img :src="product.imagenProducto" alt="" />
+          <img :src="imageComputed" alt="" />
         </div>
         <div class="px-4 md:flex-1">
           <h2 class="mb-2 text-2xl font-bold leading-tight tracking-tight text-gray-800 md:text-3xl">
-            {{ product.articulo.nombreArticulo }}
+            {{ producto.nombre }}
           </h2>
 
           <div class="my-4 flex items-center space-x-4">
             <div>
               <div class="flex rounded-lg bg-gray-100 px-3 py-2">
                 <span class="mr-1 mt-1">$</span>
-                <span class="text-3xl font-bold">{{product.precio}},000</span>
+                <span class="text-3xl font-bold">{{producto.precio}}</span>
               </div>
             </div>
+            <!--
             <div class="flex-1">
               <p class="text-xl font-semibold text-sky-600">ahorre un {{product.categoria.descuentoCategoria}}%</p>
             </div>
+            -->
           </div>
           <p class="text-gray-500">
-           {{ product.articulo.descripcion }}
+           {{ producto.descripcion }}
           </p>
 
         </div>
