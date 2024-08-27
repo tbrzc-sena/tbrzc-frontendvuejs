5a7d2165254a685d9ee33c0d3d503fc5fc53d412
tbrzc-sena <javavillabon@soy.sena.edu.co>
2024-08-26 20:27:06 -0500

    groupss!!!

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -6,7 +6,6 @@ import { useMutation } from "@vue/apollo-composable";
 import { useAuthStore } from "../../store/Auth";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
-import Toast from "primevue/toast";
 import { useQuery } from "@vue/apollo-composable";
 
 const store = useAuthStore();
@@ -37,7 +36,9 @@ const LOGGED_IN_QUERY = gql`
 `;
 
 const { mutate: data } = useMutation(LOGIN_MUTATION);
-const { result, loading, error } = useQuery(LOGGED_IN_QUERY);
+const { result, error,loading } = useQuery(LOGGED_IN_QUERY);
+
+
 
 
 const login = async () => {
@@ -48,13 +49,16 @@ const login = async () => {
     });
     if (response.data.tokenAuth.token) {
       store.setJwt(response.data.tokenAuth.token);
-      //validar el rol del usuario y redirigir a la vista correspondiente
-   
+      store.setUserRole(result.value.loggedIn.groups.edges[0].node.name)
+      //redirigir a la ruta correcta
+
+
     }
   } catch (error) {
     store.error = error;
   }
 };
+
 </script>
 <template>
   <HeaderAlternative></HeaderAlternative>
@@ -62,40 +66,57 @@ const login = async () => {
     <main>
       <div class="flex w-full justify-center">
         <div class="my-10 flex items-center px-6">
-          <div class="flex h-full items-center rounded-lg bg-white shadow-md md:rounded-l-lg md:rounded-r-none">
+          <div
+            class="flex h-full items-center rounded-lg bg-white shadow-md md:rounded-l-lg md:rounded-r-none"
+          >
             <div class="m-5 h-min">
               <h1 class="text-xl font-semibold">Bienvenido de nuevo</h1>
               <small class="">Por favor ingrese sus datos</small>
-              <form class="mt-4" @submit.prevent="login()">
+              <form class="mt-4" @submit.prevent>
                 <div class="mb-3 flex">
-                  <input id="login-email" type="email" placeholder="Email" class="block w-full rounded-md"
-                    v-model="email" />
+                  <input
+                    id="login-email"
+                    type="email"
+                    placeholder="Email"
+                    class="block w-full rounded-md"
+                    v-model="email"
+                  />
                 </div>
                 <div class="mb-3 flex">
-                  <input type="password" placeholder="Contraseña" class="block w-full rounded-md border"
-                    v-model="password" />
+                  <input
+                    type="password"
+                    placeholder="Contraseña"
+                    class="block w-full rounded-md border"
+                    v-model="password"
+                  />
                 </div>
                 <div class="mb-3 flex items-center">
                   <input id="remember" type="checkbox" class="" />
                   <label for="remember" class="mx-2">Recordarme</label>
 
-                  <RouterLink :to="{ name: 'forgotpassword' }" class="mx-2">Olvido su contraseña?</RouterLink>
+                  <RouterLink :to="{ name: 'forgotpassword' }" class="mx-2"
+                    >Olvido su contraseña?</RouterLink
+                  >
                 </div>
                 <div class="mb-3">
-                  <button type="submit" class="my-1 w-full">
+                  <button type="submit" class="my-1 w-full" @click="login()">
                     iniciar sesión
                   </button>
                 </div>
               </form>
               <div class="text-center">
                 <p class="">¿No tienes cuenta?</p>
-                <RouterLink :to="{ name: 'registerview' }">Registrarse</RouterLink>
+                <RouterLink :to="{ name: 'registerview' }"
+                  >Registrarse</RouterLink
+                >
               </div>
             </div>
           </div>
           <div class="hidden h-full rounded-r-md md:flex">
-            <img class="h-full w-full rounded-r-md bg-cover bg-center bg-no-repeat"
-              src="/public/assets/imgs/tapetes-final.webp" />
+            <img
+              class="h-full w-full rounded-r-md bg-cover bg-center bg-no-repeat"
+              src="/public/assets/imgs/tapetes-final.webp"
+            />
           </div>
         </div>
       </div>
